a892f34287da72e908127415cdd55758037947d9
1198835488@qq.com <jhdgit199312>
2019-11-08 15:13:59 +0800

    commit

---
+++ src/main/java/T1/DBPool.java
@@ -0,0 +1,58 @@
+package T1;
+
+import T1.dbpool.SqlConnectImpl;
+
+import java.sql.Connection;
+import java.util.LinkedList;
+
+/**
+ * 数据库连接池
+ */
+public class DBPool {
+
+    private static LinkedList<Connection> pool = new LinkedList<Connection>();
+
+    public DBPool(int initalSize){
+        if (initalSize <= 0){
+            throw new RuntimeException("initalSize exception ");
+        }
+        for (int i = 0;i < initalSize;i++){
+            pool.addLast(SqlConnectImpl.fetchConnection());
+        }
+    }
+
+
+    //取数据库连接
+    public Connection fetchConnection(long mills) throws InterruptedException {
+        synchronized (pool){
+            if (mills <= 0){
+                while (pool.isEmpty()){
+                    pool.wait();
+                }
+                return pool.removeFirst();
+            }else {
+                long overtime = System.currentTimeMillis() + mills;
+                long remain = mills;
+                while (pool.isEmpty() && remain > 0){
+                    pool.wait(remain);
+                    remain = System.currentTimeMillis() - overtime;
+                }
+                Connection result = null;
+                if (!pool.isEmpty()){
+                    result = pool.removeFirst();
+                }
+                return result;
+            }
+        }
+    }
+
+    //放数据库连接
+    public void releaseConn(Connection connection){
+        if (connection != null){
+            synchronized (pool){
+                pool.addLast(connection);
+                pool.notifyAll();
+            }
+        }
+    }
+}
---
+++ src/main/java/T1/dbpool/DBPoolTest.java
@@ -0,0 +1,70 @@
+package T1.dbpool;
+
+import T1.DBPool;
+
+import java.sql.Connection;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 50个线程
+ * 每个线程操作20次
+ */
+public class DBPoolTest {
+
+    static DBPool pool = new DBPool(10);
+    static  int threadCount = 50;
+    static CountDownLatch end = new CountDownLatch(threadCount);
+
+    public static void main(String[] args) throws InterruptedException {
+        int count = 20;
+        AtomicInteger got = new AtomicInteger();
+        AtomicInteger notGot = new AtomicInteger();
+        for (int i = 0;i<threadCount;i++){
+            new Thread(new Worker(count,got,notGot),"worker_"+i).start();
+        }
+        end.await();
+        System.out.println("total times ["+threadCount*count+"]");
+        System.out.println("success times ["+got+"]");
+        System.out.println("failed times ["+notGot+"]");
+    }
+
+    static class Worker implements Runnable{
+        int count;
+        AtomicInteger got;
+        AtomicInteger notGot;
+
+        public Worker(int count, AtomicInteger got, AtomicInteger notGot) {
+            this.count = count;
+            this.got = got;
+            this.notGot = notGot;
+        }
+
+        @Override
+        public void run() {
+            while (count > 0){
+                try {
+                    Connection connection = pool.fetchConnection(1000);
+                    if (connection != null){
+                        try {
+                            connection.createStatement();
+                            connection.commit();
+                        }finally {
+                            pool.releaseConn(connection);
+                            got.incrementAndGet();
+                        }
+                    }else {
+                       notGot.incrementAndGet();
+                        System.out.println(Thread.currentThread().getName()+"time out");
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    count--;
+                }
+            }
+            end.countDown();
+        }
+    }
+
+}
